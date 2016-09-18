c70f1b9a8967ced577b9c11347bde491ba077500
Vladimir Kondenko <kondenko2011@gmail.com>
2016-09-18 10:27:43 +0300

    Remove unnecesary log output

--- app/src/main/java/com/kondenko/alarmguard/utils/NotificationActionReceiver.kt
+++ app/src/main/java/com/kondenko/alarmguard/utils/NotificationActionReceiver.kt
@@ -13,10 +13,8 @@ import com.pawegio.kandroid.i
 class NotificationActionReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        i("$intent executed")
         // This is called if the user increases the alarm volume through the notification action
         if (intent?.action.equals(Constants.ACTION_INCREASE_VOLUME)) {
-            i("Increasing volume")
             context?.audioManager?.setStreamVolume(AudioManager.STREAM_ALARM, Preferences.prefMinVolume, 0)
             NotificationManagerCompat.from(context).cancel(Constants.NOTIF_LOW_VOLUME_ID)
         }
