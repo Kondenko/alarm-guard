a15f699746f225968660aa2ecf75b35e93bb1490
Vladimir Kondenko <kondenko2011@gmail.com>
2016-09-23 17:11:03 +0300

    Fix SwitchPreference not saving its state

--- app/src/main/kotlin/com/kondenko/alarmguard/preferences/PrefFragment.kt
+++ app/src/main/kotlin/com/kondenko/alarmguard/preferences/PrefFragment.kt
@@ -2,13 +2,16 @@ package com.kondenko.alarmguard.preferences
 
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.SwitchPreferenceCompat
 import com.kondenko.alarmguard.R
 
 class PrefFragment() : PreferenceFragmentCompat() {
 
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        val switchForceVolume = preferenceScreen.getPreference(1)
+        val switchForceVolume = preferenceScreen.getPreference(1) as SwitchPreferenceCompat
+        switchForceVolume.isChecked = Preferences.increaseAutomatically
         switchForceVolume.setOnPreferenceChangeListener { preference, value -> changeValue(value as Boolean) }
     }
 
