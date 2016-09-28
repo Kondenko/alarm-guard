eb6f22055519c059e1c40edaca6275ffd96b8c13
Vladimir Kondenko <kondenko2011@gmail.com>
2016-09-28 20:53:27 +0300

    Make SwitchPreference save its value using a key

--- app/src/main/kotlin/com/kondenko/alarmguard/preferences/PrefFragment.kt
+++ app/src/main/kotlin/com/kondenko/alarmguard/preferences/PrefFragment.kt
@@ -7,17 +7,8 @@ import com.kondenko.alarmguard.R
 
 class PrefFragment() : PreferenceFragmentCompat() {
 
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        val switchForceVolume = preferenceScreen.getPreference(1) as SwitchPreferenceCompat
-        switchForceVolume.isChecked = Preferences.increaseAutomatically
-        switchForceVolume.setOnPreferenceChangeListener { preference, value -> changeValue(value as Boolean) }
-    }
-
-    private fun changeValue(value: Boolean): Boolean {
-        Preferences.increaseAutomatically = value
-        return true
     }
 
 }
--- app/src/main/kotlin/com/kondenko/alarmguard/preferences/Preferences.kt
+++ app/src/main/kotlin/com/kondenko/alarmguard/preferences/Preferences.kt
@@ -1,18 +1,22 @@
 package com.kondenko.alarmguard.preferences
 
+import android.content.Context
 import com.chibatching.kotpref.KotprefModel
 import com.kondenko.alarmguard.Constants
+import com.kondenko.alarmguard.R
 
 /**
  * Manages app preferences
  */
 object Preferences : KotprefModel() {
 
+    var isFirstRun: Boolean by booleanPrefVar(default = true)
+
     // Determines if the app is checking alarm volume in the background
     var appEnabled: Boolean by booleanPrefVar(default = false)
 
     var defaultVolume: Int by intPrefVar(default = Constants.DEFAULT_ALARM_VOLUME)
 
-    var increaseAutomatically: Boolean by booleanPrefVar(default = false)
+    var increaseAutomatically: Boolean by booleanPrefVar(default = false, key = context.getString(R.string.pref_key_increase_volume))
 
 }
